641fb3b75d8403a63e6c115f6f56e3a85f6f8201
YanuarEka <yanuarekasusila@gmail.com>
2023-08-18 23:27:04 +0700

    Adding fade in components notes & form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-
+import Fade from 'react-reveal';
 export default function Form({ onAddItem }) {
   const [name, setName] = useState('');
 
@@ -21,7 +21,9 @@ export default function Form({ onAddItem }) {
 
   return (
     <div className="flex items-center justify-between sm:max-lg">
-      <h1 className="text-xl md:text-2xl underline ">What is your main focus today?</h1>
+      <Fade>
+        <h1 className="text-xl md:text-2xl underline ">What is your main focus today?</h1>
+      </Fade>
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Write note" className="border border-b-gray-400  py-1 m-2 border-transparent outline-none" autoFocus value={name} onChange={(e) => setName(e.target.value)} />
         <button className=" border-b-black bg-black text-white px-3 py-1 border">Add</button>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,13 +1,15 @@
 import Item from './Item';
-
+import Fade from 'react-reveal';
 export default function GroceryList({ items, onDeleteItem, onToggleItem }) {
   return (
     <>
-      <ul>
-        {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
-        ))}
-      </ul>
+      <Fade>
+        <ul>
+          {items.map((item) => (
+            <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
+          ))}
+        </ul>
+      </Fade>
     </>
   );
 }
